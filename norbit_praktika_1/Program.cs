b45f5d6b75f0a7b6613548d6f03c727b5dc9ef25
Vikkktorr <abobavik@gmail.com>
2025-09-10 16:55:56 +0300

    first task

---
+++ norbit_praktika_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace norbit_praktika_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string result = PercentCalculation(1000, 3, 10);
+            Console.WriteLine(result);
+        }
+
+        static string PercentCalculation(double initial_deposit, int years, int interest_rate)
+        {
+            StringBuilder result = new StringBuilder();
+            double currentAmount = initial_deposit;
+
+            for (int i = 1; i <= years; i++)
+            {
+                double percents = currentAmount * interest_rate / 100;
+                currentAmount += percents;
+                result.AppendLine($"Год {i}: {currentAmount:F2} руб.");
+            }
+
+            return result.ToString();
+        }
+    }
+}
