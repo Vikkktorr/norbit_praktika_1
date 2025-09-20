efb277a2f4de342b3fe15399060451ce88a0cf46
Vikkktorr <abobavik@gmail.com>
2025-09-20 14:57:06 +0300

    proverki

--- norbit_praktika_1/Program.cs
+++ norbit_praktika_1/Program.cs
@@ -6,24 +6,57 @@ namespace norbit_praktika_1
     {
         static void Main(string[] args)
         {
-            
+
             string result = PercentCalculation(1000, 3, 10);
             Console.WriteLine(result);
         }
 
         static string PercentCalculation(double initial_deposit, int years, int interest_rate)
         {
-            StringBuilder result = new StringBuilder();
-            double currentAmount = initial_deposit;
-
-            for (int i = 1; i <= years; i++)
+            if (initial_deposit < 0)
             {
-                double percents = currentAmount * interest_rate / 100;
-                currentAmount += percents;
-                result.AppendLine($"Год {i}: {currentAmount:F2} руб.");
+                return "Ошибка: Начальный депозит не может быть отрицательным";
             }
 
-            return result.ToString();
+            if (years < 0)
+            {
+                return "Ошибка: Количество лет не может быть отрицательным";
+            }
+
+            if (interest_rate < 0)
+            {
+                return "Ошибка: Процентная ставка не может быть отрицательной";
+            }
+
+
+            if (initial_deposit == 0)
+            {
+                return "Ошибка: Начальный депозит не может быть нулевым";
+            }
+
+            if (years == 0)
+            {
+                return "Ошибка: Количество лет не может быть нулевым";
+            }
+
+            if (interest_rate == 0)
+            {
+                return "Ошибка: Процентная ставка равна 0%, сумма не изменится";
+            }
+            else
+            {
+                StringBuilder result = new StringBuilder();
+                double currentAmount = initial_deposit;
+
+                for (int i = 1; i <= years; i++)
+                {
+                    double percents = currentAmount * interest_rate / 100;
+                    currentAmount += percents;
+                    result.AppendLine($"Год {i}: {currentAmount:F2} руб.");
+                }
+
+                return result.ToString();
+            }
         }
     }
 }
--- norbit_praktika_1/obj/Debug/net8.0/norbit_praktika_1.AssemblyInfo.cs
+++ norbit_praktika_1/obj/Debug/net8.0/norbit_praktika_1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("norbit_praktika_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b45f5d6b75f0a7b6613548d6f03c727b5dc9ef25")]
 [assembly: System.Reflection.AssemblyProductAttribute("norbit_praktika_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("norbit_praktika_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
